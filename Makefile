d3bb98b549cfea61c1c75236ba57210b2943b346
francois <fgognet@gmail.com>
2022-05-07 22:17:38 +0200

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+test-integration: 
+	go run cmd/venom-gherkin/main.go generate --input-dir test/dataset --output-dir test/got
--- cmd/venom-gherkin/generate/cmd.go
+++ cmd/venom-gherkin/generate/cmd.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var Cmd = &cobra.Command{
@@ -12,10 +13,40 @@ var Cmd = &cobra.Command{
 	Long:    `venom-gherkin generate`,
 	Aliases: []string{"g"},
 	Run: func(cmd *cobra.Command, args []string) {
+		cmd.LocalFlags().VisitAll(initFromCommandArguments)
 		generateFiles()
 	},
 }
 
+var (
+	inputDir  string
+	outputDir string
+
+	inputDirFlag  *string
+	outputDirFlag *string
+)
+
+func init() {
+	inputDirFlag = Cmd.PersistentFlags().String("input-dir", "input", "Input Directory: taking gherkin files from this directory")
+	outputDirFlag = Cmd.PersistentFlags().String("output-dir", "output", "Output Directory: writing venom files to this directory")
+}
+func initFromCommandArguments(f *pflag.Flag) {
+	if !f.Changed {
+		return
+	}
+
+	switch f.Name {
+	case "input-dir":
+		if inputDirFlag != nil {
+			inputDir = *inputDirFlag
+		}
+	case "output-dir":
+		if outputDirFlag != nil {
+			outputDir = *outputDirFlag
+		}
+	}
+}
+
 func generateFiles() {
-	fmt.Println("Generate files")
+	fmt.Printf("Generate files from %s to %s", inputDir, outputDir)
 }
